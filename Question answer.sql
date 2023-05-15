8431b4a5b4ceb61313e5a26315fde2a32f9ecb92
Dev Malhotra <59054044+dev-10@users.noreply.github.com>
2023-05-15 15:58:43 +0530

    Add files via upload

---
+++ Question answer.sql
@@ -0,0 +1,17 @@
+
+create Table Employees
+(
+  id int primary key identity,
+  Name varchar(50) NOT NULL,
+  Salary money NOT NULL,
+  Manager_Id int
+  )
+  drop table Employees;
+  select * from Employees;
+  Insert into Employees values('Rohit',20000,3);
+  Insert into Employees values('Sangeeta',12000,5);
+  Insert into Employees values('Sanjay',10000,5);
+  Insert into Employees values('Arun',25000,3);
+  Insert into Employees values('Zaheer',30000,NULL);
+
+ Select  employee.name, manager.name as Manager_name FROM Employees employee LEFT JOIN Employees manager ON employee.Manager_Id = manager.Id order by employee.name
